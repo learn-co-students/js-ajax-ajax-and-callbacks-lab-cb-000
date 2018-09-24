c6813552fc4b80de7724c3ae7b933a3ffa6513ed
Heather Christman <efl7a@yahoo.de>
2018-09-24 13:16:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 $(document).ready(function (){
 });
+function searchRepositories() {
+  var searchTerms = document.getElementById('searchTerms').value
+  var url = "https://api.github.com/search/repositories?q=" + searchTerms
+  $.get(url, function(data){
+    var src = document.getElementById("repo-template").innerHTML
+    const template = Handlebars.compile(src)
+    const repoList = template(data.items)
+    $("#results").html(repoList)
+  }).fail(error => displayError());
+}
+function displayError(error) {
+  $("#errors").html("I'm sorry, there's been an error. Please try again")
+}
+
+function showCommits(el) {
+  var url ="https://api.github.com/repos/"+ el.dataset.owner + "/"+ el.dataset.repository + "/commits"
+  $.get(url, function(data) {
+    var src = document.getElementById("commits-template").innerHTML
+    const template = Handlebars.compile(src)
+    const commitsList = template(data)
+//    var list = `<ul>${data.map(item => '<li>' + item.commit.author.name + '<br>' + item.sha + '<img src="' + item.committer.avatar_url + '" width="32" height= "25"></li>').join('')}</ul>`
+    $("#details").html(commitsList)
+  });
+}
