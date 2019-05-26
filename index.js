aacf57d0b6c32df28e04ef3f543e589443143db9
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-26 20:34:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 function displayError() {
-  $("#errors").html('<strong>I'm sorry, there's been an error. Please try again.</strong>');
+  $("#errors").html("<strong>I'm sorry, there's been an error. Please try again.</strong>");
 }
 
 function searchRepositories() {
-  const searchTerms = $('#searchTerms').value.split(' ').join('+');
-  $(document).ready(function (){
+  const searchTerms = $('#searchTerms').get(0).value.split(' ').join('+');
+  $(document).ready(function () {
     $.get('https://api.github.com/search/repositories?q=' + searchTerms, function(response) {
-      $("#results").html('<ul>');
+      let htmlString = '<ul>';
       $.each(response.items, function(idx, item) {
-        $("#results").html('<li><a href="' + item.html_url + '">' + item.name + '</a> - Description: ' + item.description + ' - Owner: <a href="https://github.com/' + item.owner.login + '">' + item.owner.login + '<img height="32" width="32" src="' + item.owner.avatar_url + '"></a> - <a href="#" onclick="showCommits(this)" data-repository="' + item.name + '" data-username="' + item.owner.login + '">Show Commits</a></li>');
+        htmlString = htmlString + '<li><a href="' + item.html_url + '">' + item.name + '</a> - Description: ' + item.description + ' - Owner: <a href="https://github.com/' + item.owner.login + '">' + item.owner.login + '<img height="32" width="32" src="' + item.owner.avatar_url + '"></a> - <a href="#" onclick="showCommits(this)" data-repository="' + item.name + '" data-username="' + item.owner.login + '">Show Commits</a></li>';
       });
-      $("#results").html('</ul>');
+      htmlString += '</ul>';
+      $("#results").html(htmlString);
     }).fail(displayError);
   });
 }
@@ -18,11 +19,14 @@ function searchRepositories() {
 function showCommits(el) {
   const name = el.dataset.repository;
   const username = el.dataset.username;
-  $.get('https://api.github.com/repos/' + username + '/' + name + '/commits', function(commits) {
-    $("#details").html('<ul>');
-    $.each(commits, function(idx, commit) {
-      $("#details").html('<li>SHA: ' + commit.sha + '- Author: ' + commit.author.login + ' - Avatar: <img height="32" width="32" src="' + commit.author.avatar_url + '"></li>');
-    });
-    $("#details").html('</ul>');
-  }).fail(displayError);
+  $(document).ready(function () {
+    $.get('https://api.github.com/repos/' + username + '/' + name + '/commits', function(commits) {
+      let htmlString = '<ul>';
+      $.each(commits, function(idx, commit) {
+        htmlString = htmlString + '<li>SHA: ' + commit.sha + '- Author: ' + commit.author.login + ' - Avatar: <img height="32" width="32" src="' + commit.author.avatar_url + '"></li>';
+      });
+      htmlString += '</ul>';
+      $("#details").html(htmlString);
+    }).fail(displayError);
+  });
 }
